9153ef3b5c6708b9376e9c3e5d34b80a9d7307fc
James Gibbons <jamesfngibbons@icloud.com>
2015-10-20 09:46:00 +0100

    Initial Commit

---
+++ mongo_function_class.py
@@ -0,0 +1,24 @@
+from pymongo import MongoClient
+from datetime import datetime
+
+'''
+Licence: Copyright Centiq Ltd
+Author: W.Overton <woverton@centiq.co.uk>
+Note:  Connect to the MongoDB server using the config file parameters.
+'''
+
+class mongoConnectionEngine:
+
+	def getCurrentTime(self):
+		return int(datetime.now().microsecond)
+
+	def connect(self, address, port):
+		startTime = self.getCurrentTime()
+		try:
+			self.client = MongoClient(address, int(port))
+			self.client.server_info()
+			return (str(0), "Connected OK", str(float(self.getCurrentTime() - startTime)/1000))
+		except Exception as e:
+			#connection unsuccesful
+			return (str(1), str(e), str(float(self.getCurrentTime() - startTime)/1000))
+		
---
+++ mongo_status_checker.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+
+'''
+Licence: Copyright Centiq Ltd
+Author: W.Overton <woverton@centiq.co.uk>
+Note:   Check local instance of MongoDB. Config file takes 2 parameters, host and port under block 'Mongo_Server'.
+'''
+
+import ConfigParser
+from datetime import datetime
+from mongo_function_class import *
+
+class main:
+	def __init__(self):
+		self.outputHeader()
+		self.config = ConfigParser.RawConfigParser()
+		self.config.read("config.conf")
+		self.getMongoConnectionDetals()
+		self.outputRowData()
+	
+	def outputRowData(self):
+		mongo_Host = self.config.get("Mongo_Server","Hostname")
+		mongo_Port = self.config.get("Mongo_Server","Port")
+		serverInfo = self.mongoConnectionEngine.connect(mongo_Host,mongo_Port)
+		print(self.config.get("Mongo_Server", "Hostname") + "@@@" + self.config.get("Mongo_Server", "Port") + "@@@" + serverInfo[0] + "@@@" + serverInfo[1] + "@@@" + serverInfo[2])
+
+	def outputHeader(self):
+		print("ma_name: mongo_status")
+		print("ma_group_name: Information")
+		print("ma_short_name: mgst")
+		print("ma_mon_order: 30")
+		print("ma_group_column: RemoteAddress")
+		print("ma_keys: RemoteAddress @@@ Port")
+		print("ma_data:")
+		print("Remote Address @@@ Port @@@ Status @@@ Info @@@ Connection Time")
+
+	#get mongo config data  from config and send it to the connection class
+	def getMongoConnectionDetals(self):
+		mongo_Host = self.config.get("Mongo_Server","Hostname")
+		mongo_Port = self.config.get("Mongo_Server","Port")
+		self.mongoConnectionEngine = mongoConnectionEngine()
+		self.mongoConnectionEngine.connect(mongo_Host, mongo_Port)
+main()
+
+
